e6497358414ec78cb36f8feb7496a329cbb0630e
PramodRaoB <pramod.b@research.iiit.ac.in>
2024-12-17 15:10:44 +0530

    Clean up merging iterator implementation

--- src/main/java/picard/sam/markduplicates/util/MergingIteratorForTwoSortingLongCollections.java
+++ src/main/java/picard/sam/markduplicates/util/MergingIteratorForTwoSortingLongCollections.java
@@ -5,82 +5,50 @@ import htsjdk.samtools.util.*;
 import java.util.*;
 
 /**
- * Very specific utility class that can merge at most two sorting long collections
+ * Very specific utility class that can merge sorting long collections
+ * Switch to a priority queue based implementation if required.
+ * Currently since its only being used to merge two collections, performance wise it does not make sense
  */
 public class MergingIteratorForTwoSortingLongCollections {
-    SortingLongCollectionForMarkDuplicates first, second;
-    boolean firstDone, secondDone;
+    List<SortingLongCollectionForMarkDuplicates> cols;
+    List<Boolean> done;
     private static final Log log = Log.getInstance(MergingIteratorForTwoSortingLongCollections.class);
 
-    public MergingIteratorForTwoSortingLongCollections(SortingLongCollectionForMarkDuplicates first, SortingLongCollectionForMarkDuplicates second) {
-        this.first = first;
-        this.second = second;
-        if (this.first.hasNext()) firstDone = false;
-        else {
-            firstDone = true;
-            first.cleanup();
-        }
-        if (this.second.hasNext()) secondDone = false;
-        else {
-            secondDone = true;
-            second.cleanup();
+    public MergingIteratorForTwoSortingLongCollections(SortingLongCollectionForMarkDuplicates... args) {
+        log.debug(String.format("Creating a merging iterator for Sorting long collections"));
+        cols = new ArrayList<>();
+        done = new ArrayList<>();
+        for (SortingLongCollectionForMarkDuplicates arg: args) {
+            if (arg.hasNext()) {
+                cols.add(arg);
+                done.add(false);
+            } else arg.cleanup();
         }
     }
 
-    public MergingIteratorForTwoSortingLongCollections(SortingLongCollectionForMarkDuplicates first) {
-        this.first = first;
-        this.second = null;
-        if (this.first.hasNext()) firstDone = false;
-        else {
-            firstDone = true;
-            first.cleanup();
-        }
-        secondDone = true;
-    }
-
-    public boolean hasNext() {return !firstDone || !secondDone;}
+    public boolean hasNext() {return done.stream().anyMatch(n -> n);}
 
     public long next() {
         if (!hasNext()) throw new NoSuchElementException();
-        long val = 0;
-        if (!firstDone && !secondDone) {
-            if (first.peek() <= second.peek()) {
-                val = first.next();
-                if (!first.hasNext()) {
-                    firstDone = true;
-                    first.cleanup();
-                }
-            } else {
-                val = second.next();
-                if (!second.hasNext()) {
-                    secondDone = true;
-                    second.cleanup();
-                }
-            }
-        } else if (!firstDone) {
-            val = first.next();
-            if (!first.hasNext()) {
-                firstDone = true;
-                first.cleanup();
-            }
-        } else {
-            val = second.next();
-            if (!second.hasNext()) {
-                secondDone = true;
-                second.cleanup();
-            }
+        long val = -1;
+        int best = -1;
+        for (int idx = 0; idx < cols.size(); idx++) if (!done.get(idx) && (val == -1 || cols.get(idx).peek() < val)) {
+            val = cols.get(idx).peek();
+            best = idx;
+        }
+        SortingLongCollectionForMarkDuplicates col = cols.get(best);
+        val = col.next();
+        if (!col.hasNext()) {
+            done.set(best, true);
+            col.cleanup();
         }
         return val;
     }
 
     public void cleanup() {
-        if (!firstDone) {
-            firstDone = true;
-            first.cleanup();
-        }
-        if (!secondDone) {
-            secondDone = true;
-            second.cleanup();
+        for (int idx = 0; idx < cols.size(); idx++) if (!done.get(idx)) {
+            done.set(idx, true);
+            cols.get(idx).cleanup();
         }
     }
 }
