61eeebd9e4601d3633a272c3a3d4f8c8e0956725
PramodRaoB <pramod.b@research.iiit.ac.in>
2024-12-19 23:18:33 +0530

    Revert "Try mmap for read end codec"
    This reverts commit ac5d44f941b9a5cac417f25e3c396c41bbae5f75.

--- src/main/java/picard/sam/markduplicates/util/ReadEndsForMarkDuplicatesCodec.java
+++ src/main/java/picard/sam/markduplicates/util/ReadEndsForMarkDuplicatesCodec.java
@@ -1,274 +1,117 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2009 The Broad Institute
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package picard.sam.markduplicates.util;
 
 import htsjdk.samtools.util.SortingCollection;
 import picard.PicardException;
 
 import java.io.*;
-import java.nio.BufferUnderflowException;
-import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
-import java.nio.channels.WritableByteChannel;
 
-/**
- * Optimized codec for ReadEnds that uses memory mapping for improved performance.
- * Falls back to standard I/O for non-file streams.
- */
+/** Codec for ReadEnds that just outputs the primitive fields and reads them back. */
 public class ReadEndsForMarkDuplicatesCodec implements SortingCollection.Codec<ReadEndsForMarkDuplicates> {
-    private static final int CHUNK_SIZE = 64 * 1024 * 1024; // 64MB chunks
-    private static final int WRITE_BUFFER_SIZE = 8 * 1024 * 1024; // 8MB write buffer
+    protected DataInputStream in;
+    protected DataOutputStream out;
 
-    // Input handling
-    private FileChannel inputChannel;
-    private ByteBuffer mappedInputBuffer;
-    private DataInputStream standardInput;
-    private long currentInputPosition = 0;
-
-    // Output handling
-    private FileChannel outputChannel;
-    private ByteBuffer writeBuffer;
-    private DataOutputStream standardOutput;
-
-    // Record size tracking for pre-allocation
-    private static final int ESTIMATED_RECORD_SIZE = 50; // Approximate size in bytes
-
-    @Override
     public SortingCollection.Codec<ReadEndsForMarkDuplicates> clone() {
         return new ReadEndsForMarkDuplicatesCodec();
     }
 
-    @Override
-    public void setOutputStream(final OutputStream os) {
-        if (os instanceof FileOutputStream) {
-            try {
-                outputChannel = ((FileOutputStream) os).getChannel();
-                writeBuffer = ByteBuffer.allocateDirect(WRITE_BUFFER_SIZE);
-            } catch (Exception e) {
-                // Fall back to standard output
-                outputChannel = null;
-                standardOutput = new DataOutputStream(new BufferedOutputStream(os, WRITE_BUFFER_SIZE));
-            }
-        } else {
-            standardOutput = new DataOutputStream(new BufferedOutputStream(os, WRITE_BUFFER_SIZE));
-        }
+    public void setOutputStream(final OutputStream os) { this.out = new DataOutputStream(os); }
+
+    public void setInputStream(final InputStream is) { this.in = new DataInputStream(is); }
+
+    public DataInputStream getInputStream() {
+        return in;
     }
 
-    @Override
-    public void setInputStream(final InputStream is) {
-        if (is instanceof FileInputStream) {
-            try {
-                inputChannel = ((FileInputStream) is).getChannel();
-                mapNextInputChunk();
-            } catch (IOException e) {
-                throw new PicardException("Failed to initialize memory-mapped input", e);
-            }
-        } else {
-            standardInput = new DataInputStream(new BufferedInputStream(is, WRITE_BUFFER_SIZE));
-        }
+    public DataOutputStream getOutputStream() {
+        return out;
     }
 
-    private void mapNextInputChunk() throws IOException {
-        if (currentInputPosition >= inputChannel.size()) {
-            mappedInputBuffer = null;
-            return;
-        }
-
-        long remainingBytes = inputChannel.size() - currentInputPosition;
-        long chunkSize = Math.min(CHUNK_SIZE, remainingBytes);
-
-        mappedInputBuffer = inputChannel.map(
-                FileChannel.MapMode.READ_ONLY,
-                currentInputPosition,
-                chunkSize
-        );
-        currentInputPosition += chunkSize;
-    }
-
-    private void ensureWriteBufferCapacity(int required) throws IOException {
-        if (writeBuffer.remaining() < required) {
-            writeBuffer.flip();
-            outputChannel.write(writeBuffer);
-            writeBuffer.clear();
-        }
-    }
-
-    @Override
     public void encode(final ReadEndsForMarkDuplicates read) {
         try {
-            if (outputChannel != null) {
-                encodeToBuffer(read);
-            } else {
-                encodeToStream(read);
+            this.out.writeShort(read.score);
+            this.out.writeShort(read.libraryId);
+            this.out.writeByte(read.orientation);
+            this.out.writeInt(read.read1ReferenceIndex);
+            this.out.writeInt(read.read1Coordinate);
+            this.out.writeLong(read.read1IndexInFile);
+            this.out.writeInt(read.read2ReferenceIndex);
+
+            if (read.orientation > ReadEnds.R) {
+                this.out.writeInt(read.read2Coordinate);
+                this.out.writeLong(read.read2IndexInFile);
             }
-        } catch (IOException ioe) {
+
+            this.out.writeShort(read.readGroup);
+            this.out.writeShort(read.tile);
+            this.out.writeShort((short)read.x);
+            this.out.writeShort((short)read.y);
+            this.out.writeByte(read.orientationForOpticalDuplicates);
+            this.out.writeInt(read.duplicateSetSize);
+            this.out.writeByte(read.firstOfFlag);
+            this.out.writeInt(read.windowIndex);
+        } catch (final IOException ioe) {
             throw new PicardException("Exception writing ReadEnds to file.", ioe);
         }
     }
 
-    private void encodeToBuffer(final ReadEndsForMarkDuplicates read) throws IOException {
-        // Ensure we have enough space
-        ensureWriteBufferCapacity(ESTIMATED_RECORD_SIZE);
-
-        // Write all fields to buffer
-        writeBuffer.putShort(read.score);
-        writeBuffer.putShort(read.libraryId);
-        writeBuffer.put(read.orientation);
-        writeBuffer.putInt(read.read1ReferenceIndex);
-        writeBuffer.putInt(read.read1Coordinate);
-        writeBuffer.putLong(read.read1IndexInFile);
-        writeBuffer.putInt(read.read2ReferenceIndex);
-
-        if (read.orientation > ReadEnds.R) {
-            writeBuffer.putInt(read.read2Coordinate);
-            writeBuffer.putLong(read.read2IndexInFile);
-        }
-
-        writeBuffer.putShort(read.readGroup);
-        writeBuffer.putShort(read.tile);
-        writeBuffer.putShort((short)read.x);
-        writeBuffer.putShort((short)read.y);
-        writeBuffer.put(read.orientationForOpticalDuplicates);
-        writeBuffer.putInt(read.duplicateSetSize);
-        writeBuffer.put(read.firstOfFlag);
-        writeBuffer.putInt(read.windowIndex);
-    }
-
-    private void encodeToStream(final ReadEndsForMarkDuplicates read) throws IOException {
-        standardOutput.writeShort(read.score);
-        standardOutput.writeShort(read.libraryId);
-        standardOutput.writeByte(read.orientation);
-        standardOutput.writeInt(read.read1ReferenceIndex);
-        standardOutput.writeInt(read.read1Coordinate);
-        standardOutput.writeLong(read.read1IndexInFile);
-        standardOutput.writeInt(read.read2ReferenceIndex);
-
-        if (read.orientation > ReadEnds.R) {
-            standardOutput.writeInt(read.read2Coordinate);
-            standardOutput.writeLong(read.read2IndexInFile);
-        }
-
-        standardOutput.writeShort(read.readGroup);
-        standardOutput.writeShort(read.tile);
-        standardOutput.writeShort((short)read.x);
-        standardOutput.writeShort((short)read.y);
-        standardOutput.writeByte(read.orientationForOpticalDuplicates);
-        standardOutput.writeInt(read.duplicateSetSize);
-        standardOutput.writeByte(read.firstOfFlag);
-        standardOutput.writeInt(read.windowIndex);
-    }
-
-    @Override
     public ReadEndsForMarkDuplicates decode() {
+        final ReadEndsForMarkDuplicates read = new ReadEndsForMarkDuplicates();
         try {
-            if (inputChannel != null) {
-                return decodeFromBuffer();
-            } else {
-                return decodeFromStream();
+            // If the first read results in an EOF we've exhausted the stream
+            try {
+                read.score = this.in.readShort();
+            } catch (final EOFException eof) {
+                return null;
             }
-        } catch (EOFException eof) {
-            return null;
-        } catch (IOException ioe) {
-            throw new PicardException("Exception reading ReadEnds from file.", ioe);
-        }
-    }
 
-    private ReadEndsForMarkDuplicates decodeFromBuffer() throws IOException {
-        // Check if we need to map next chunk
-        if (mappedInputBuffer == null || !mappedInputBuffer.hasRemaining()) {
-            mapNextInputChunk();
-            if (mappedInputBuffer == null) {
-                return null; // EOF
-            }
-        }
-
-        try {
-            final ReadEndsForMarkDuplicates read = new ReadEndsForMarkDuplicates();
-
-            read.score = mappedInputBuffer.getShort();
-            read.libraryId = mappedInputBuffer.getShort();
-            read.orientation = mappedInputBuffer.get();
-            read.read1ReferenceIndex = mappedInputBuffer.getInt();
-            read.read1Coordinate = mappedInputBuffer.getInt();
-            read.read1IndexInFile = mappedInputBuffer.getLong();
-            read.read2ReferenceIndex = mappedInputBuffer.getInt();
+            read.libraryId = this.in.readShort();
+            read.orientation = this.in.readByte();
+            read.read1ReferenceIndex = this.in.readInt();
+            read.read1Coordinate = this.in.readInt();
+            read.read1IndexInFile = this.in.readLong();
+            read.read2ReferenceIndex = this.in.readInt();
 
             if (read.orientation > ReadEnds.R) {
-                read.read2Coordinate = mappedInputBuffer.getInt();
-                read.read2IndexInFile = mappedInputBuffer.getLong();
+                read.read2Coordinate = this.in.readInt();
+                read.read2IndexInFile = this.in.readLong();
             }
 
-            read.readGroup = mappedInputBuffer.getShort();
-            read.tile = mappedInputBuffer.getShort();
-            read.x = mappedInputBuffer.getShort();
-            read.y = mappedInputBuffer.getShort();
-            read.orientationForOpticalDuplicates = mappedInputBuffer.get();
-            read.duplicateSetSize = mappedInputBuffer.getInt();
-            read.firstOfFlag = mappedInputBuffer.get();
-            read.windowIndex = mappedInputBuffer.getInt();
+            read.readGroup = this.in.readShort();
+            read.tile = this.in.readShort();
+            read.x = this.in.readShort();
+            read.y = this.in.readShort();
+
+            read.orientationForOpticalDuplicates = this.in.readByte();
+            read.duplicateSetSize = this.in.readInt();
+            read.firstOfFlag = this.in.readByte();
+            read.windowIndex = this.in.readInt();
 
             return read;
-        } catch (BufferUnderflowException e) {
-            // If we hit the end of the buffer mid-record, map next chunk and retry
-            mapNextInputChunk();
-            if (mappedInputBuffer == null) {
-                return null; // EOF
-            }
-            return decodeFromBuffer();
-        }
-    }
-
-    private ReadEndsForMarkDuplicates decodeFromStream() throws IOException {
-        final ReadEndsForMarkDuplicates read = new ReadEndsForMarkDuplicates();
-
-        try {
-            read.score = standardInput.readShort();
-        } catch (EOFException eof) {
-            return null;
-        }
-
-        read.libraryId = standardInput.readShort();
-        read.orientation = standardInput.readByte();
-        read.read1ReferenceIndex = standardInput.readInt();
-        read.read1Coordinate = standardInput.readInt();
-        read.read1IndexInFile = standardInput.readLong();
-        read.read2ReferenceIndex = standardInput.readInt();
-
-        if (read.orientation > ReadEnds.R) {
-            read.read2Coordinate = standardInput.readInt();
-            read.read2IndexInFile = standardInput.readLong();
-        }
-
-        read.readGroup = standardInput.readShort();
-        read.tile = standardInput.readShort();
-        read.x = standardInput.readShort();
-        read.y = standardInput.readShort();
-        read.orientationForOpticalDuplicates = standardInput.readByte();
-        read.duplicateSetSize = standardInput.readInt();
-        read.firstOfFlag = standardInput.readByte();
-        read.windowIndex = standardInput.readInt();
-
-        return read;
-    }
-
-    /**
-     * Closes any open resources and ensures all data is written.
-     */
-    public void close() throws IOException {
-        if (writeBuffer != null && writeBuffer.position() > 0) {
-            writeBuffer.flip();
-            outputChannel.write(writeBuffer);
-        }
-
-        if (standardOutput != null) {
-            standardOutput.flush();
-            standardOutput.close();
-        }
-
-        if (inputChannel != null) {
-            inputChannel.close();
-        }
-
-        if (standardInput != null) {
-            standardInput.close();
+        } catch (final IOException ioe) {
+            throw new PicardException("Exception writing ReadEnds to file.", ioe);
         }
     }
 }
